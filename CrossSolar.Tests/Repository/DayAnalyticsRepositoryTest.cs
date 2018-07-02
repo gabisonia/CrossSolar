9778134ad07a1ac4977351f9f0b646fd89a41fb5
gabisonia <irakli.gabisonia@outlook.com>
2018-07-03 00:09:02 +0400

    added DayAnalyticsRepositoryTest

---
+++ CrossSolar.Tests/Repository/DayAnalyticsRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using CrossSolar.Domain;
+using CrossSolar.Repository;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CrossSolar.Tests.Repository
+{
+    public class DayAnalyticsRepositoryTest : RepositoryTestBase
+    {
+        private readonly DayAnalyticsRepository _repository;
+
+        public DayAnalyticsRepositoryTest()
+        {
+            _repository = new DayAnalyticsRepository(_crossSolarDbContext);
+        }
+
+        [Fact]
+        public async Task GetGetHistoryTest()
+        {
+            var panelId = 1;
+
+            var result = await _repository.GetHistory(panelId);
+
+            Assert.NotNull(result);
+        }
+    }
+}
